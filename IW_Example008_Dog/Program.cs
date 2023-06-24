b1bdbb309ab2ea2d43f297abf559e4d695c5aa6a
Alexandr25041973 <sashan25041973@yandex.ru>
2023-06-24 19:41:41 +0300

    Add Independent Work Example 008 Dog

---
+++ IW_Example008_Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+Boolean friend = true; 
+double time = 0;
+
+while(distance > 10)
+{
+  if(friend == false)
+  {
+    time = distance / (firstFriendSpeed + dogSpeed);
+    friend = true;
+  }
+  else
+  {
+    time = distance / (secondFriendSpeed + dogSpeed);
+    friend = false;
+  }
+  
+  distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+  count++;
+}
+
+Console.WriteLine(count);
